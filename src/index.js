3f8fed1f4e8587f69e70a9016a73459d96aa523c
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-05-22 22:02:32 -0700

    Added YouTube API Key and installed on NPM YouTube Search API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = "AIzaSyCd2sTy9r_kREEh7D_PrQMV04-1pDJNjVs";
+
+
 //Create a new component which should produce some HTML
 const App = () => {
     return <div>Hi!</div>;
